a007415739b412f22aaf53cb72c639b951d36871
Alexey Popov <alexey.popov@umbrella-web.com>
2018-05-02 23:55:22 +0300

    updated range operator

--- range/index.js
+++ range/index.js
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/range';
 
-const rangeObservable = Observable.range(1, 9);
+const rangeObservable = Observable.range(0, 16);
 rangeObservable.subscribe((e) => console.log(`Range emited ${e}`));
 
