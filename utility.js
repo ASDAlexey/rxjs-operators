8ffb6958a5c700075abac89b21341ef1cf966883
Alexey Popov <alexey.popov@umbrella-web.com>
2018-05-07 21:38:25 +0300

    #21 fromStdIn utility

---
+++ utility.js
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+
+export const fromStdIn = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+
+  const observable = Observable.fromEvent(stdin, 'data');
+  observable.subscribe((key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+
+  return observable;
+};
+
+fromStdIn().subscribe((a) => console.log(a));
