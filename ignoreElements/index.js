5b6c7aa1636c39af3eca59fe684438e503275422
Alexey Popov <alexey.popov@umbrella-web.com>
2018-05-09 20:22:42 +0300

    #28 ignoreElements

--- ignoreElements/index.js
+++ ignoreElements/index.js
@@ -1,7 +1,6 @@
 import { interval } from 'rxjs/observable/interval';
 import { ignoreElements } from 'rxjs/operators/ignoreElements';
 import { take } from 'rxjs/operators/take';
-import { tap } from 'rxjs/operators/tap';
 
 console.log('Interval\'s started');
 interval(100)
