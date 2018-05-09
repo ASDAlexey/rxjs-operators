1c6c05265a501c032241547b795ad0342dbbf647
Alexey Popov <alexey.popov@umbrella-web.com>
2018-05-09 20:07:44 +0300

    #26 concatMap

---
+++ concatMap/index.js
@@ -0,0 +1,11 @@
+import { interval } from 'rxjs/observable/interval';
+import { range } from 'rxjs/observable/range';
+import { concatMap } from 'rxjs/operators/concatMap';
+import { concatMapTo } from 'rxjs/operators/concatMapTo';
+
+interval(100)
+// .pipe(map(n => `Range #${n}`))
+//   .pipe(concatMap(n => range(0, 2)))
+  // .pipe(concatMap(n => range(0, n + 1)))
+  .pipe(concatMapTo(range(0, 2)))
+  .subscribe((e) => console.log(e));
