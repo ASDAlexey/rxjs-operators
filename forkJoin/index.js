90d050957a3eb44d0c01d4e156672cfb5cd17d5f
Alexey <asdalexey@yandex.ru>
2018-05-13 18:38:34 +0300

    #41 forkJoin

---
+++ forkJoin/index.js
@@ -0,0 +1,10 @@
+import { forkJoin } from 'rxjs/observable/forkJoin';
+import { timer } from 'rxjs/observable/timer';
+import { mapTo, concatAll } from 'rxjs/operators';
+
+const friendAPI = timer(1000).pipe(mapTo({ name: 'Hank' }));
+const preferencesAPI = timer(1700).pipe(
+  mapTo({ theme: 'Dark' }),
+);
+
+forkJoin(friendAPI, preferencesAPI).subscribe((e) => console.log(e));
